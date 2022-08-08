5c40d8cb013ab6319eb0982fce32d4816145ff66
Юлия Алексина <ulchez@mail.ru>
2022-08-08 21:57:58 +0300

    Добавьте файлы проекта.

---
+++ Lesson17/Count.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    public class Count<T>
+    {
+        private T Account { get; set; }
+        private double BalanceSheet { get; set; }
+        private string FullName { get; set; }
+        
+        public Count(T account, double balancesheet, string fullname)
+        {
+            Account = account;
+            BalanceSheet = balancesheet;
+            FullName = fullname;
+        }
+        public string GetInfo()
+        {
+            return $"{Account} {BalanceSheet} {FullName}";
+        }
+    }
+}
---
+++ Lesson17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числовой номер счета");
+            int accountint = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите числовое значение баланса");
+            double balanceSheet = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            string fullName = Console.ReadLine();
+            Count<int> count1 = new Count<int>(accountint,balanceSheet,fullName);
+            Console.WriteLine(count1.GetInfo());
+
+            Console.WriteLine("Введите строковый номер счета");
+            string accountstring = Console.ReadLine();
+            Console.WriteLine("Введите числовое значение баланса");
+            balanceSheet = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            fullName = Console.ReadLine();
+            Count<string> count2 = new Count<string>(accountstring,balanceSheet,fullName);
+            Console.WriteLine(count2.GetInfo());
+            Console.ReadKey();
+        }   
+    }
+}
